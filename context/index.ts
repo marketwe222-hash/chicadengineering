99e5c9e410c08acddff53ddb7b3592271d454798
obedbolak <jamesnormankate@gmail.com>
2026-05-15 19:11:02 +0100

    latest

--- context/index.ts
+++ context/index.ts
@@ -1,5 +1,5 @@
 export { ThemeProvider, useThemeContext } from "./ThemeContext";
 export { ToastProvider, useToastContext } from "./ToastContext";
 export { AuthProvider, useAuthContext } from "./AuthContext";
-export type { AuthUser, UserRole } from "./AuthContext";
+
 export type { Toast, ToastType } from "./ToastContext";
